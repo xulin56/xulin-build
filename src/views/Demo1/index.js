64e6769a7ade9571f236f88ba37d082485054392
夏江楠 <mokar@xiajiangnandeiMac.local>
2018-10-29 17:38:36 +0800

    加入公告组件

--- src/views/Demo1/index.js
+++ src/views/Demo1/index.js
@@ -5,6 +5,7 @@ import Calendar from 'components/Calendar';
 import SubNav from 'components/SubNav';
 import 'rc-pagination/assets/index.css';
 import 'rc-select/assets/index.css';
+import ScrollNotice from 'components/ScrollNotice';
 
 @autobind
 export default class Demo1 extends React.Component{
@@ -12,7 +13,8 @@ export default class Demo1 extends React.Component{
         super(props);
         this.state={
             count : 70,
-            selectDateVal : '请选择日期'
+            selectDateVal : '请选择日期',
+            dataList : [{text:'2017年2月28日 - 而且专门用来监控对象属性变化的Object.observe方法',link:'/demo'}]
         };
     }
 
@@ -32,7 +34,7 @@ export default class Demo1 extends React.Component{
         })
     }
     render(){
-        const {count,selectDateVal} = this.state;
+        const {count,selectDateVal,dataList} = this.state;
         return(
             <div className="demo1">
               <Pagination
@@ -46,6 +48,9 @@ export default class Demo1 extends React.Component{
                 />
               <Calendar selectCb={this.selectDate} selectDateVal={selectDateVal}></Calendar>
               <div style={{width:"500px"}}><SubNav></SubNav></div>
+                <ScrollNotice
+                    dataList={dataList}
+                />
             </div>
         )
     }
