322900189bd5af98becd8681f41b442d6985ef86
何旭林 <xulin@192.168.0.102>
2019-01-09 11:21:00 +0800

    暂时干掉国际语言组件

--- src/store.js
+++ src/store.js
@@ -2,12 +2,10 @@ import thunkMiddleware from 'redux-thunk';
 import { applyMiddleware,createStore,combineReducers } from 'redux';
 import {APPreducers} from 'views/App/reducers';
 import { demoReducers } from 'views/Demo/reducers';
-import {I18nReducers} from 'components/I18n/reducers';
 
 const rootReducer = combineReducers({
     APPreducers,
     demoReducers,
-    I18nReducers
 });
 export default createStore(
     rootReducer,
--- src/views/Demo/index.js
+++ src/views/Demo/index.js
@@ -5,9 +5,7 @@ import {connect} from 'react-redux';
 import API from 'js/api';
 import * as actionCreators from './actions';
 import HSelect from 'components/HSelect';
-import I18n from 'components/I18n';
 import store from 'src/store';
-import {getLang} from 'components/I18n/actions';
 import {sStore} from 'js';
 
 @autobind
@@ -25,40 +23,6 @@ export class Demo extends React.Component {
           }
       });
     }
-    change(item){
-        const {dispatch} = this.props;
-        console.log(this.props)
-        switch(item.value){
-            case 'zh' :
-                store.dispatch(getLang('zh'));
-                this.setState({
-                    selectVal : '中文'
-                });
-                sStore.set('lang','zh');
-            break;
-            case 'en' :
-                store.dispatch(getLang('en'));
-                this.setState({
-                    selectVal : '英文'
-                });
-                sStore.set('lang','en');
-            break;
-            case 'fa' :
-                store.dispatch(getLang('fa'));
-                this.setState({
-                    selectVal : '法语'
-                });
-                sStore.set('lang','fa');
-            break;
-            default :
-                store.dispatch(getLang('en'))
-                this.setState({
-                    selectVal : '英文'
-                });
-                sStore.set('lang','en');
-        }
-
-    };
     render() {
         const {selectVal} = this.state;
         return <div className='demo'>
@@ -66,18 +30,12 @@ export class Demo extends React.Component {
             <div>{this.props.index}</div>
             <button onClick={()=>this.props.add(1)}>add</button>
             <button onClick={()=>this.props.mul(3)}>mul</button>
-              <button><I18n message={'HELLO'}></I18n></button>
-              <h4><I18n message={'SHIGE'}></I18n></h4>
-              <HSelect
-                        val={selectVal}
-                        selectCb={this.change}
-                        selectList={[{label:'中文',value:"zh"},{label:'英文',value:"en"},{label:'法语',value:'fa'}]}></HSelect>
           </div>
     }
 }
 
 const mapStateToProps = (state) => {
-    return Object.assign(state.demoReducers.toJS(),state.I18nReducers.toJS());
+    return state.demoReducers.toJS()
 };
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators(actionCreators, dispatch);
